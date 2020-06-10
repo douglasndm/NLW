811205fd04497da14ea2e5313c74600f41df17aa
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-06-09 22:12:18 -0300

    Fim do quarto dia do NLW

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+
+server.use(express.static("public"));
+
+// utilizando template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+})
+
+server.get('/', function(req, res) {
+    return res.render("index.html", {
+        title: "VSF"
+    });
+});
+
+server.get('/create-point', (req, res) => {
+    return res.render("create-point.html")
+});
+
+server.get('/search', (req, res) => {
+    return res.render("search-results.html");
+});
+
+server.listen(3000);
